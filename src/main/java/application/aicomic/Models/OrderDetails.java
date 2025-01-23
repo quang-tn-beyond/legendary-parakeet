dcda5a2f0f65aeada2a5a7c5b19071f307c3fbed
annatole <chauthingoctuyen24@gmail.com>
2025-01-23 15:15:22 +0700

    Database code first

---
+++ src/main/java/application/aicomic/Models/OrderDetails.java
@@ -0,0 +1,34 @@
+package application.aicomic.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Table(name = "OrderDetails")
+@Data
+public class OrderDetails {
+    @Id
+    @Column(name = "order_detail_id", length = 50)
+    private String orderDetailId;
+
+    @NotNull
+    @Column(name = "order_id", length = 50)
+    private String orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "order_id", insertable = false, updatable = false)
+    private Orders orders;
+
+    @NotNull
+    @Column(name = "chapter_id", length = 50)
+    private String chapterId;
+
+    @ManyToOne
+    @JoinColumn(name = "chapter_id", referencedColumnName = "chapter_id", insertable = false, updatable = false)
+    private Chapters chapter;
+
+    @NotNull
+    @Column(name = "price")
+    private double price;
+}
---
+++ src/main/java/application/aicomic/Models/Users.java
@@ -0,0 +1,87 @@
+package application.aicomic.Models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+@Data
+public class Users {
+    @Id
+    @Column(name = "user_id", length = 50)
+    private String userId;
+
+    @NotNull
+    @Column(name = "user_name", length = 50)
+    private String userName;
+
+    @NotNull
+    @Column(name = "password", length = 255)
+    private String password;
+
+    @NotNull
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @NotNull
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "gender", length = 10)
+    private String gender;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "phone_number", length = 10)
+    private String phoneNumber;
+
+    @Column(name = "avatar_url", length = 255)
+    private String avatarUrl;
+
+    @NotNull
+    @Column(name = "role")
+    private byte role;
+
+    @Column(name = "status")
+    private byte status;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comics> comics;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comments> comments;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comments> reports;
+
+    @OneToMany(mappedBy = "user")
+    private List<Wallets> wallets;
+
+    @OneToMany(mappedBy = "user")
+    private List<CurrentChapter> purchasedComics;
+
+    @OneToMany(mappedBy = "user")
+    private List<Orders> orders;
+
+    @OneToMany(mappedBy = "user")
+    private List<PurchasedCoins> purchasedCoins;
+
+    @OneToMany(mappedBy = "user")
+    private List<CurrentChapter> currentChapters;
+
+    @ManyToMany(mappedBy = "user")
+    private List<Bookshelves> bookshelves;
+
+    /*@OneToMany(mappedBy = "user")
+    private List<Orders> orders;*/
+}
+

d5a043a383fe67eceef9929cee632f6d3d10a30d
PhamNgocTrungNhan <nhanpntse18224@fpt.edu.vn>
2025-02-13 06:37:17 +0700

    CRUD
    CRUD Comics, Orders, Wallets

---
+++ src/main/java/application/aicomic/repositories/ComicsRepository.java
@@ -0,0 +1,16 @@
+
+package application.aicomic.repositories;
+
+import application.aicomic.models.Comics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ComicsRepository extends JpaRepository<Comics, String> {
+    List<Comics> findByRole(byte role);
+
+    Optional<Comics> findByComicsName(String comicsName);
+
+    List<Comics> findByStatusIn(List<Byte> statuses);
+}
